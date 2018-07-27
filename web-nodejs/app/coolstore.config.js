769d4a771c60cb4b2ad4b64bdfcaf8fe31b1c517
Jorge Morales Pou <jorgemoralespou@users.noreply.github.com>
2018-07-27 18:07:21 +0200

    Web-ui uses KUBERNETES_NAMESPACE for gw config

--- web-nodejs/app/coolstore.config.js
+++ web-nodejs/app/coolstore.config.js
@@ -1,5 +1,5 @@
 var config = {
-    API_ENDPOINT: 'gateway-' + process.env.OPENSHIFT_BUILD_NAMESPACE,
+    API_ENDPOINT: 'gateway-' + process.env.KUBERNETES_NAMESPACE,
     SECURE_API_ENDPOINT: 'secure-gateway-' + process.env.SECURE_COOLSTORE_GW_SERVICE,
     SSO_ENABLED: process.env.SSO_URL ? true : false
 };
@@ -7,13 +7,13 @@ var config = {
 if (process.env.COOLSTORE_GW_ENDPOINT != null) {
     config.API_ENDPOINT = process.env.COOLSTORE_GW_ENDPOINT;
 } else if (process.env.COOLSTORE_GW_SERVICE != null) {
-    config.API_ENDPOINT = process.env.COOLSTORE_GW_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE;
+    config.API_ENDPOINT = process.env.COOLSTORE_GW_SERVICE + '-' + process.env.KUBERNETES_NAMESPACE;
 }
 
 if (process.env.SECURE_COOLSTORE_GW_ENDPOINT != null) {
     config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_ENDPOINT;
 } else if (process.env.SECURE_COOLSTORE_GW_SERVICE != null) {
-    config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE;
+    config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_SERVICE + '-' + process.env.KUBERNETES_NAMESPACE;
 }
 
 module.exports = config;
